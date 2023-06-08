f8aff2ae5443d65bca8d026e1c971b78fed4c7e2
Jlalo01 <110356514+Jlalo01@users.noreply.github.com>
2023-06-08 09:22:28 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,22 +6,49 @@ function App() {
   const [x, setX] = useState("Z")
   const [run, setRun] = useState(false)
   const [rand, setRand] = useState()
-
-  function yClick(){
-    setRand(Math.floor(Math.random() * 100));
-    
-  }
+  const [s, setS] = useState(-1)
 
   useEffect(() =>{
-    if (rand%2 === 0){setX("A");}
-    else if (rand%2 === 1){setX("B");}
-    if (x !== "Z"){
-      setRun(true)
+    if (s === 0){
+      if (rand%2 === 0){setX("A");}
+      else if (rand%2 === 1){setX("B");}
+      if (x !== "Z"){
+        setRun(true);
+      }
     }
-  }, [rand, x])
+    else if (s === 1){
+      if (rand / 10 < 1){setX("A");}
+      else if (rand / 20 < 1){setX("C");}
+      else {setX("B");}
+      if (x !== "Z"){setRun(true);}
+
+    }
+  }, [rand, x, s])
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  function yClick(){
+    setS(0);
+    setRand(Math.floor(Math.random() * 100));
+  }
+
+  function hClick(){
+    const t =  Math.floor(Math.random() * 2000);
+    sleep(t)
+    .then(() => {setRand(Math.floor(Math.random() * 100));});
+  }
+
+  function tClick(){
+    setS(1);
+    const r = Math.floor(Math.random() * 30);
+    setRand(r);
+  }
 
   function re(){
-    window.location.reload(false);
+   //window.location.reload(false);
+   setRun(false);
   }
 
 
@@ -48,13 +75,13 @@ function App() {
         </div>
   
         <div className='choice'>
-          <div className='button'>
+          <div className='button' onClick={tClick}>
             <div className='btnTxt'>1/3</div>
           </div>
         </div>
   
         <div className='choice'>
-          <div className='button'>
+          <div className='button' onClick={hClick}>
             <div className='btnTxt'>Heavy</div>
           </div>
         </div>
